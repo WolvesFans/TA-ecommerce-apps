91de6e5f5621efb9bd8a58adc932611a0fed13ac
WolvesFans <zakipermaditriputra@gmail.com>
2025-02-04 17:35:27 +0700

    update convertToIdr function

--- lib/utils/helpers/pricing_calculator.dart
+++ lib/utils/helpers/pricing_calculator.dart
@@ -1,3 +1,5 @@
+import 'package:intl/intl.dart';
+
 class CPricingCalculator {
   //nanti cari api buat perkiraan shipping cost
   static double getShippingCost(String location) {
@@ -10,6 +12,16 @@ class CPricingCalculator {
     double shippingCost = getShippingCost(location);
     double totalPrice = productPrice + shippingCost;
     return totalPrice;
+  }
+
+  //convert semuanya ke rupiah
+  static String convertToIdr(dynamic number, int decimalDigit) {
+    NumberFormat currencyFormatter = NumberFormat.currency(
+      locale: 'id',
+      symbol: 'Rp ',
+      decimalDigits: decimalDigit,
+    );
+    return currencyFormatter.format(number);
   }
 
   //jumlahin harga di keranjang
