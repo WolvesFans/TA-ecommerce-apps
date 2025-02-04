e20b516704b8b5d67a84b4ee91fb31944a88a964
WolvesFans <zakipermaditriputra@gmail.com>
2025-02-04 17:44:52 +0700

    update the widget using convertToIdr function

--- lib/common/widgets/texts/product_price_text.dart
+++ lib/common/widgets/texts/product_price_text.dart
@@ -1,16 +1,16 @@
+import 'package:comro_alt/utils/helpers/pricing_calculator.dart';
 import 'package:flutter/material.dart';
 
 class CProductPriceText extends StatelessWidget {
   const CProductPriceText({
     super.key,
-    required this.currencySign,
     required this.price,
     this.maxLines = 1,
     this.isLarge = false,
     this.lineThrough = false,
   });
 
-  final String currencySign, price;
+  final dynamic price;
   final int maxLines;
   final bool isLarge;
   final bool lineThrough;
@@ -18,7 +18,7 @@ class CProductPriceText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Text(
-      currencySign + price,
+      CPricingCalculator.convertToIdr(price, 0),
       maxLines: maxLines,
       overflow: TextOverflow.ellipsis,
       style: isLarge
